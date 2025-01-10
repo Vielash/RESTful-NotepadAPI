1e8eaffa0bde47148967e72e5f79238540e34ed7
Vielash <engino0505@gmail.com>
2025-01-10 03:16:12 +0300

    Looking for exception handling

---
+++ src/main/java/notepadingo/exception/NoteNotFoundException.java
@@ -0,0 +1,17 @@
+package notepadingo.exception;
+
+public class NoteNotFoundException extends RuntimeException {
+
+    public NoteNotFoundException() {
+        super("Note does not found");
+    }
+
+    public NoteNotFoundException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+
+    public NoteNotFoundException(String exceptionMessage, Throwable chainCause) { //I learned this new for example there is 2 exceptions named A and B. If A is thrown
+        super(exceptionMessage, chainCause);                                 //and caused to program throw B exception you write A exception object in B's constructor.
+    }
+
+}
--- src/main/java/notepadingo/service/NoteService.java
+++ src/main/java/notepadingo/service/NoteService.java
@@ -1,4 +1,98 @@
 package notepadingo.service;
 
-public class NoteService {
+import notepadingo.exception.NoteNotFoundException;
+import notepadingo.model.Note;
+import notepadingo.repository.NoteRepository;
+
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.Set;
+
+
+public class NoteService implements INoteService {
+
+    NoteRepository repository = new NoteRepository();
+
+
+    @Override
+    public Note getNoteByTitle(String noteTitle) throws NoteNotFoundException, IllegalArgumentException {
+        String titleWithoutGap = noteTitle.trim();
+        if(noteTitle.length() > 45) {
+            throw new IllegalArgumentException("Note's title can not be more than 45 character.");
+        }
+        if(noteTitle == null || titleWithoutGap.isEmpty()) {
+            throw new IllegalArgumentException("Note's title can not be empty or null");
+        }
+        try {
+            Note choosenNote = repository.getNoteByTitle(noteTitle);
+            if (choosenNote == null) {
+                throw new NoteNotFoundException("Note with" + noteTitle + "title not found.");
+            }
+            return choosenNote;
+        }
+        catch (ConcurrentModificationException e){
+            throw new RuntimeException("You got error while accessing the notes.");
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Unexpected ERROR.");
+        }
+    }
+
+    @Override
+    public Set<String> getAllNoteTitles() throws ConcurrentModificationException {
+        Set<String> allNoteTitles = repository.getAllNoteTitles();
+
+        try {
+            return allNoteTitles;
+        }
+        catch(ConcurrentModificationException e) {
+            throw new RuntimeException("You got error while accessing the notes or notes didnt even created.");
+        }
+    }
+
+    @Override
+    public Collection<Note> getAllNotes() {
+        return null;
+    }
+
+    @Override
+    public boolean addNote(String newTitle, String newContent) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteNoteByTitle(String noteTitle) {
+        return false;
+    }
+
+    @Override
+    public boolean updateNoteContent(String checkTitle, String newContent) {
+        return false;
+    }
+
+    @Override
+    public boolean updateNoteTitle(String checkTitle, String newTitle) {
+        return false;
+    }
+
+    @Override
+    public boolean doesNoteExist(String noteTitle) {
+        return false;
+    }
+
+    @Override
+    public Note getLongestTitleNote() {
+        return null;
+    }
+
+    @Override
+    public boolean addNoteIfContentValid(String noteTitle, String content) {
+        return false;
+    }
+
+    @Override
+    public List<Note> findNotesByKeyword(String keyword) {
+        return null;
+    }
 }
